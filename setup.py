a04fd326d00b0b092cc404cf8e1a3fc35a37d595
dominiek <d.dierickx@gmail.com>
2013-06-22 13:37:10 +0200

    ready for initial commit

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(
 	name='subr',
       version='0.1',
-      description='Automagically find subtitles for video files.',
+      description='Automagically find subtitles for video files from bierdopje.com (dutch).',
       author='Dominique Dierickx',
       author_email='d.dierickx@gmail.com',
       url='http://github.com/ddierickx/subr.git',
